bd761e41d6957d4a04c80e7b39badd51e7ce19c0
StarJins <kinjn9495@naver.com>
2022-11-23 22:26:44 +0900

    엑셀 함수 파이썬으로 이식 완료

--- mahjong_score_board.py
+++ mahjong_score_board.py
@@ -6,6 +6,8 @@ import json
 import exception_class
 import traceback
 
+# TODO: 함수들 class화 해서 캡슐화 진행하기
+
 def openSpreadsheet():
     with open('info.json', 'r', encoding='UTF-8') as json_file:
         info = json.load(json_file)
@@ -20,14 +22,14 @@ def openSpreadsheet():
         spreadsheet_name = info['spreadsheetName']
         client = gspread.authorize(creds)
         spreadsheet = client.open(spreadsheet_name)
-        
+
         return spreadsheet
 
 def printRank():
     spreadsheet = openSpreadsheet()
     sheet = spreadsheet.worksheet('순위')
     ranks = sheet.get_all_records()
-    
+
     # 순위, 이름, 점수
     for key in ranks[0].keys():
         print(key, end = '\t')
@@ -50,8 +52,10 @@ def getFullName(name):
         return '김진태'
     elif name in '박인수':
         return '박인수'
-    else: # name in '서준석'
+    elif name in '서준석':
         return '서준석'
+    else:
+        raise exception_class.invalidName
 
 def getNameIdx(name):
     if name in '권혁규':
@@ -67,7 +71,7 @@ def getNameIdx(name):
     elif name in '서준석':
         return 7
     else:
-        return -1
+        raise exception_class.invalidName
     
 def getCellAlphaByIdx(idx):
     if idx == 2:
@@ -83,10 +87,7 @@ def getCellAlphaByIdx(idx):
     else: #idx == 7
         return 'H'
 
-def insertMahjongScore(input_data):
-    # 마작 점수표 업데이트
-    
-    # 변수 설정
+def paringInputData(input_data):
     split_input_data = input_data.split()
     if len(split_input_data) != 9:
         raise exception_class.invalidInput
@@ -95,12 +96,28 @@ def insertMahjongScore(input_data):
     name_list = [split_input_data[1], split_input_data[3], split_input_data[5], split_input_data[7]]
     score_list = [split_input_data[2], split_input_data[4], split_input_data[6], split_input_data[8]]
 
+    return wind, name_list, score_list
+
+def setWind(wind):
+    if wind == '동장' or wind == '동풍' or wind == '동':
+        return '동장'
+    elif wind == '반장' or wind == '반' or wind == '남' or wind == '남풍':
+        return '반장'
+    else:
+        raise exception_class.invalidWind
+
+def insertMahjongScore(input_data):
+    # 마작 점수표 업데이트
+
+    # 변수 설정
+    wind, name_list, score_list = paringInputData(input_data)
+
     # 구글 시트 접속
     spreadsheet = openSpreadsheet()
     sheet = spreadsheet.worksheet('마작 점수표')
     total_row_index = len(sheet.get_all_values())
     total_row_index += 1
-    
+
     # 마작 점수표 sheet 셀 순서
     # A: 날짜, B: 동장/반장, C: 권, D: 마, E: 재, F: 진, G: 인, H: 준, I: 총합, J: 동남서북
     insert_data = [0 for i in range(10)]
@@ -110,66 +127,92 @@ def insertMahjongScore(input_data):
     date = datetime.today()
     ymd = '{}-{}-{}'.format(date.year, date.month, date.day)
     insert_data[0] = ymd
-    
+
     # B: 동장/반장
-    if wind == '동장' or wind == '동풍' or wind == '동':
-        wind = '동장'
-    elif wind == '반장' or wind == '반' or wind == '남' or wind == '남풍':
-        wind == '반장'
-    else:
-        raise exception_class.invalidWind
-    insert_data[1] = wind
+    wind = setWind(wind)
+    insert_data[1] = setWind(wind)
 
     # C ~ H: 점수 입력
     total_score = 0
     for i in range(4):
         idx = getNameIdx(name_list[i])
-        if idx == -1 or check_array[idx] == 1:
+        if check_array[idx] == 1:
             raise exception_class.invalidName
         else:
             insert_data[idx] = score_list[i]
             total_score += int(score_list[i])
             check_array[idx] = 1
-            
+
     # I: 총합
     if total_score != 100000:
         raise exception_class.invalidTotalScore
     insert_data[8] = total_score
-    
+
     # J: 동남서북
     insert_data[9] = '동:{}, 남:{}, 서:{}, 북:{}'.format(getFullName(name_list[0]), getFullName(name_list[1]), getFullName(name_list[2]), getFullName(name_list[3]))
-    
+
     # data 입력
     sheet.append_row(values=insert_data, value_input_option=gspread.utils.ValueInputOption.user_entered)
 
-def insertUmaScore():
+def insertUmaScore(input_data):
     # 우마 점수표 업데이트
+
+    # 변수 설정
+    wind, name_list, score_list = paringInputData(input_data)
 
     # 구글 시트 접속
     spreadsheet = openSpreadsheet()
     sheet = spreadsheet.worksheet('우마 점수표')
     total_row_index = len(sheet.get_all_values())
     total_row_index += 1
-    
+
     # 우마 점수표 sheet 셀 순서
     # A: 날짜, B: 동장/반장, C: 권, D: 마, E: 재, F: 진, G: 인, H: 준, I: 총합, J: 동남서북
     insert_data = [0 for i in range(10)]
-    
+
     # A: 날짜
-    insert_data[0] = "='마작 점수표'!A{}".format(total_row_index)
-    
+    date = datetime.today()
+    ymd = '{}-{}-{}'.format(date.year, date.month, date.day)
+    insert_data[0] = ymd
+
     # B: 동장/반장
-    insert_data[1] = "='마작 점수표'!B{}".format(total_row_index)
-    
+    wind = setWind(wind)
+    insert_data[1] = wind
+
     # C ~ H: 점수 입력
-    for idx in range(2, 8):
-        insert_data[idx] = """=IF(RANK('마작 점수표'!{col_index}{row_index}, '마작 점수표'!$C{row_index}:$H{row_index}) + COUNTIF('마작 점수표'!$C${row_index}:'마작 점수표'!{col_index}{row_index},'마작 점수표'!{col_index}{row_index}) - 1 = 1, ('마작 점수표'!{col_index}{row_index} / 1000) + IF('마작 점수표'!B{row_index} = "반장", '원투우마'!$A$1, 10), IF(RANK('마작 점수표'!{col_index}{row_index}, '마작 점수표'!$C{row_index}:$H{row_index}) + COUNTIF('마작 점수표'!$C${row_index}:'마작 점수표'!{col_index}{row_index},'마작 점수표'!{col_index}{row_index}) - 1 = 2, ('마작 점수표'!{col_index}{row_index} / 1000) + IF('마작 점수표'!B{row_index} = "반장", '원투우마'!$B$1, 5), IF(RANK('마작 점수표'!{col_index}{row_index}, '마작 점수표'!$C{row_index}:$H{row_index}) + COUNTIF('마작 점수표'!$C${row_index}:'마작 점수표'!{col_index}{row_index},'마작 점수표'!{col_index}{row_index}) - 1 = 3, ('마작 점수표'!{col_index}{row_index} / 1000) + IF('마작 점수표'!B{row_index} = "반장", '원투우마'!$C$1, -5), IF(RANK('마작 점수표'!{col_index}{row_index}, '마작 점수표'!$C{row_index}:$H{row_index}) + COUNTIF('마작 점수표'!$C${row_index}:'마작 점수표'!{col_index}{row_index},'마작 점수표'!{col_index}{row_index}) - 1 = 4, ('마작 점수표'!{col_index}{row_index} / 1000) + IF('마작 점수표'!B{row_index} = "반장", '원투우마'!$D$1, -10), 0))))""".format(col_index=getCellAlphaByIdx(idx),row_index=total_row_index)
+    # 원투 우마 점수 상수화
+    ONE_TWO_UMA_SCORE = { '반장' : { 1 : 20, 2 : 10, 3 : -10, 4 : -20 }, '동장' : { 1 : 10, 2 : 5, 3 : -5, 4 : -10 } }
+
+    # 마작 점수 + 원투우마 순으로 정렬
+    # 이때 마작 점수가 동점인 경우, 동남서북 순서로 원투우마 기록
+    result_score = {}
+    priority_idx = 1
+    for idx in range(4):
+        result_score[getFullName(name_list[idx])] = [int(int(score_list[idx]) / 1000), priority_idx]
+        priority_idx += 1
+    # -item[1][0] -> score(내림차순), item[1][1] -> priority_idx(오름차순), tuple로 만들어서 이중 정렬 구현
+    result_score = dict(sorted(result_score.items(), key = lambda item : (-item[1][0], item[1][1])))
+
+    rank = 1
+    for key in result_score.keys():
+        result_score[key].append(rank)
+        rank += 1
+
+    for key in result_score.keys():
+        result_score[key][0] += ONE_TWO_UMA_SCORE[wind][result_score[key][2]]
+
+    total_uma = 0
+    for key in result_score.keys():
+        insert_data[getNameIdx(key)] = result_score[key][0]
+        total_uma += result_score[key][0]
 
     # I: 총합
-    insert_data[8] = '=sum(C{row_index}:H{row_index})'.format(row_index=total_row_index)
+    if total_uma != 100:
+        raise exception_class.invalidTotalScore
+    insert_data[8] = total_uma
     
     # J: 동남서북
-    insert_data[9] = "='마작 점수표'!J{}".format(total_row_index)
+    insert_data[9] = '동:{}, 남:{}, 서:{}, 북:{}'.format(getFullName(name_list[0]), getFullName(name_list[1]), getFullName(name_list[2]), getFullName(name_list[3]))
 
     # data 입력
     sheet.append_row(values=insert_data, value_input_option=gspread.utils.ValueInputOption.user_entered)
@@ -180,56 +223,31 @@ def updateRawData():
     # 구글 시트 접속
     spreadsheet = openSpreadsheet()
     sheet = spreadsheet.worksheet('우마 점수표')
-    total_row_index = len(sheet.get_all_values())   # 추가된 row index 구하기
+    total_data = sheet.get_all_values()
 
     sheet = spreadsheet.worksheet('RAW 데이터')
-    
-    # 추가된 row index를 총점 계산에 반영
+
+    # 각 유저의 uma 총합을 계산해서 update
     for idx in range(2, 8):
+        total_score = 0
+        for elem in total_data[1:]:
+            total_score += int(elem[idx])
         cellidx = '{col_index}2'.format(col_index=getCellAlphaByIdx(idx))
-        value = "=SUM('우마 점수표'!{col_index}2:{col_index}{row_index})".format(col_index=getCellAlphaByIdx(idx), row_index=total_row_index)
-        sheet.update(cellidx, value, value_input_option=gspread.utils.ValueInputOption.user_entered)
-    
+        sheet.update(cellidx, total_score, value_input_option=gspread.utils.ValueInputOption.user_entered)
 
 if __name__ == '__main__':
     try:
-        try:
-            print('점수 입력(ex: 반장 인 15000 홍 30000 진 20000 준 35000)')
-            print('사람/점수는 동남서북 순으로 입력')
-            test_input_score = input()
-            insertMahjongScore(test_input_score)
-        except exception_class.invalidInput as e:
-            print(e)
-        except exception_class.invalidWind as e:
-            print(e)
-        except exception_class.invalidName as e:
-            print(e)
-        except exception_class.invalidTotalScore as e:
-            print(e)
-        except ValueError as e:
-            print(e)
-            print('형식에 맞춰 점수를 숫자로 입력해 주세요.')
-        else:
-            insertUmaScore()
-            updateRawData()
-            printRank()
-    except gspread.exceptions.APIError as e:
+        print('점수 입력(ex: 반장 인 20000 홍 20000 진 20000 준 40000)')
+        print('사람/점수는 동남서북 순으로 입력')
+        test_input_score = input()
+        insertMahjongScore(test_input_score)
+        insertUmaScore(test_input_score)
+        updateRawData()
+        printRank()
+    except ValueError as e:
         print(e)
-    except gspread.exceptions.CellNotFound as e:
-        print(e)
-    except gspread.exceptions.GSpreadException as e:
-        print(e)
-    except gspread.exceptions.IncorrectCellLabel as e:
-        print(e)
-    except gspread.exceptions.InvalidInputValue as e:
-        print(e)
-    except gspread.exceptions.NoValidUrlKeyFound as e:
-        print(e)
-    except gspread.exceptions.SpreadsheetNotFound as e:
-        print(e)
-    except gspread.exceptions.UnSupportedExportFormat as e:
-        print(e)
-    except gspread.exceptions.WorksheetNotFound as e:
+        print('형식에 맞춰 점수를 숫자로 입력해 주세요.')
+    except Exception as e:
         print(e)
     except:
         traceback.print_exc()
