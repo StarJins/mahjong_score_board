3ed05707dc18370b7a22ad33f528942c1d9caad2
StarJins <kinjn9495@naver.com>
2022-12-03 17:15:10 +0900

    작혼주소 추가

--- mahjong_score_bot.py
+++ mahjong_score_bot.py
@@ -150,9 +150,13 @@ async def showMahjongScore(ctx):
     embed.add_field(name='동남서북', value=order_data)
     await ctx.send(embed=embed)
 
-@client.command(name='링크')    # 구글 스프레드시트 링크 출력
+@client.command(name='DB링크')    # 구글 스프레드시트 링크 출력
 async def showRank(ctx):
     await ctx.send('https://docs.google.com/spreadsheets/d/1H736VRbaNlfG92llYu0I5qZPfkr92-C6w_Q1VBBlzgM/edit?usp=sharing')
+
+@client.command(name='작혼링크')    # 작혼 주소 출력
+async def showRank(ctx):
+    await ctx.send('https://game.mahjongsoul.com/index.html')
 
 @client.command(name='help')
 async def showCommand(ctx):
@@ -162,7 +166,8 @@ async def showCommand(ctx):
         '!마작점수' : '현재 저장된 마작 점수 전체 출력',
         '!우마점수' : '현재 저장된 우마 점수 전체 출력',
         '!입력 풍 사람1 점수1 사람2 점수2 사람3 점수3 사람4 점수4' : '경기 결과 입력(ex 반장 인 20000 홍 20000 진 20000 준 40000), 사람/점수는 동남서북 순으로 입력',
-        '!링크' : '원투우마 엑셀 링크 출력',
+        '!DB링크' : '원투우마 엑셀 링크 출력',
+        '!작혼링크' : '작혼주소 출력',
     }
 
     for k, v in dict_commands.items():
