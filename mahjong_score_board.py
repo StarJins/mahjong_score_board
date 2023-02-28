aa83e9440aadf8da3cfdc5fc822ea690e29f934b
StarJins <kinjn9495@naver.com>
2023-03-01 01:33:46 +0900

    fix bug

--- mahjong_score_board.py
+++ mahjong_score_board.py
@@ -172,7 +172,7 @@ class MahjongScoreBoardController:
         result_score = {}
         priority_idx = 1
         for idx in range(4):
-            result_score[self.__getFullName(name_list[idx])] = [int(int(score_list[idx]) / 1000), priority_idx]
+            result_score[self.__getFullName(name_list[idx])] = [int(score_list[idx]), priority_idx]
             priority_idx += 1
         # -item[1][0] -> score(내림차순), item[1][1] -> priority_idx(오름차순), tuple로 만들어서 이중 정렬 구현
         result_score = dict(sorted(result_score.items(), key = lambda item : (-item[1][0], item[1][1])))
@@ -191,7 +191,7 @@ class MahjongScoreBoardController:
             total_uma += result_score[key][0]
 
         # I: 총합
-        if total_uma != 100:
+        if total_uma != 100000:
             raise exception_class.invalidTotalScore
         insert_data[8] = total_uma
         
